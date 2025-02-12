470e172598e5c547faf7ee41ecfae2b6de1467b8
Arthur Lopes Oliveira <129803691+Arthurestak@users.noreply.github.com>
2025-02-12 16:06:24 -0300

    Add files via upload

---
+++ O.O.P.py
@@ -0,0 +1,14 @@
+# Cria Classe
+class Funcionarios:
+    pass
+
+# Cria o Objeto
+usuario = Funcionarios()
+
+# Criar os parametros
+usuario.nome = 'Arthur'
+usuario.sobrenome = 'Oliveira'
+usuario.data_de_nascimento = '14/02/2007'
+
+# Print
+print(usuario)
